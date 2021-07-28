ce6f7baa25ac435f4af3c2c22b38fc35f93720b9
dontloo <wdl.stan@gmail.com>
2021-07-27 23:09:41 -0700

    Update scale.py

--- scale.py
+++ scale.py
@@ -3,10 +3,8 @@
 from fractions import Fraction
 import math
 
-def lcm(a, b):
-    return abs(a*b) // math.gcd(a, b)
-
 c = '1'
+d = '9/8'
 e = '5/4'
 f = '4/3'
 g = '3/2'
@@ -17,15 +15,21 @@ c_hi = '2'
 cg = [c, g]
 ceg = [c, e, g]
 fac = [f, a, c_hi]
-freq = [Fraction(x) for x in cg]
+maj_penta = [c, d, e, g, a]  # d/a = 27/40
+cegfa = [c, e, f, g, a]  # e/f = 15/16
+freq = [Fraction(x) for x in cegfa]
 
 max_cycle = 1
-for x in freq:
-    for y in freq:
-        r = x/y
+max_r = 0
+min_r = 1
+for i in range(len(freq)):
+    for j in range(i + 1, len(freq)):
+        r = freq[i]/freq[j]
+        max_r = max(r, max_r)
+        min_r = min(r, min_r)
         cycle = r.numerator*r.denominator
         if cycle > max_cycle:
             max_cycle = cycle
-            print(x, y, r, lcm(x,y))
+            print(freq[i], freq[j], r)
             
-print(max_cycle)
+print(max_cycle, max_r, min_r)
